eb6286da84cc46b638f0b8df8e6be330a6e4043f
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-08-19 08:02:21 +0300

    daroma

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router';
+import { ChooseService } from './pages/ChooseService';
+import { Supirkimas } from './pages/Supirkimas';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Switch>
+          <Route exact path="/supirkimas" component={Supirkimas} />
+          <Route exact path="/" component={ChooseService} />
+        </Switch>
+      </div>
     </div>
   );
 }
