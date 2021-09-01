7b6613c14c864fa7cf2193bc9854dfe543352e41
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-09-01 17:59:45 +0300

    burger menu

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -2,10 +2,36 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const MainNav = () => {
+  const showLinks = () => {
+    let links = document.getElementsByClassName('burger-links');
+    console.log(links);
+    links[0].classList.toggle('show-links');
+  };
   return (
     <nav className="main-nav">
       <div className="logo">
         <h3 className="logo-text">NIJORA</h3>
+      </div>
+      <div className="burger-nav">
+        <i
+          onClick={showLinks}
+          class="fa fa-bars burger-nav-icon"
+          aria-hidden="true"
+        ></i>
+        <div className="burger-links">
+          <Link className="burger-nav__single-link" to="/">
+            I pagrindini meniu
+          </Link>
+          <a className="burger-nav__single-link" href="#top">
+            Pradzia
+          </a>
+          <a className="burger-nav__single-link" href="#services">
+            Paslaugos
+          </a>
+          <a className="burger-nav__single-link" href="#contacts">
+            Kontaktai
+          </a>
+        </div>
       </div>
       <div className="main-nav__links">
         <Link className="main-nav__single-link" to="/">
